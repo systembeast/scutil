51219ffde80d9d2de41668bee4f3897d308a75a8
dingbig <dingbig@gmail.com>
2019-10-15 11:47:32 +0800

    add new scutil

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "scutil"
 version := "0.1"
 
 scalaVersion := "2.13.1"
+
+libraryDependencies += "org.json" % "json" % "20190722"
--- src/main/scala/com/sysbeast/scutil/ClassUtil.scala
+++ src/main/scala/com/sysbeast/scutil/ClassUtil.scala
@@ -15,13 +15,17 @@ object ClassUtil {
         Some(Class.forName(s))
       } catch {
         case e: Throwable =>
+          println("load failed " + s)
           e.printStackTrace()
           None
       }
     }
   }
 
+
+
   def findClasses[T](cls: Class[T], loc: File):  List[Class[_ <:T]] = {
+    println("findClasses ", loc)
     if(loc.isDirectory)
       findClasses(cls, loc.deepFindExt(".class").map(_.getAbsolutePath).map(_.substring(loc.getAbsolutePath.length + 1)).map(_.dropRight(".class".length)).map(_.replace('/', '.')).map(_.replace('\\','.')))
     else
@@ -30,6 +34,8 @@ object ClassUtil {
 
   def findClasses[T](cls: Class[T], jar: JarFile):  List[Class[_ <:T]] = findClasses(cls,jar.entries().asScala.toList.map(_.getName).map(_.replace('/','.')).filter(_.endsWith(".class")).map(_.dropRight(".class".length)))
   def findClasses[T](cls: Class[T], names: List[String]):  List[Class[_ <:T]] = names
+    .filterNot(_.contains("module-info"))
+    .filterNot(_.contains("org.eclipse"))
     .map(_.loadClass)
     .filter(_.isDefined)
     .map(_.get)
